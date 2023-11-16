36b11513d56ab969c34abaf44709db2fc4ccb0d7
DMOVIES <150237980+DMOVIES@users.noreply.github.com>
2023-11-16 17:22:07 +0530

    er

--- plugins/commands.py
+++ plugins/commands.py
@@ -310,7 +310,9 @@ async def start(client, message):
             file_id = file.file_id
             files_ = await get_file_details(file_id)
             files1 = files_[0]
-            title = ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files1.file_name.split()))
+            title = '@Doffice_bots ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files1.file_name.split()))
+
+
             size=get_size(files1.file_size)
             f_caption=files1.caption
             if CUSTOM_FILE_CAPTION:
