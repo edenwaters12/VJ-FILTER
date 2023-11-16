a0cdb6f97fd7b336203ba25c3668960ce1479f4f
DMOVIES <150237980+DMOVIES@users.noreply.github.com>
2023-11-16 10:33:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/DMOVIES/VJ-FILTER.git /VJ-FILTER 
+  git clone https://github.com/DMOVIES/VJ-FILTER.git /dmovies
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /VJ-FILTER 
